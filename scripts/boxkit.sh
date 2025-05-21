4761808597ee29e41e89d4c6a90612cbba4473b3
zdyxry <zdyxry@gmail.com>
2025-05-21 17:12:23 +0800

    chore: remove boxkit image

--- scripts/boxkit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Symlink distrobox shims
-./distrobox-shims.sh
-
-# Update the container and install packages
-apk update && apk upgrade
-grep -v '^#' ./boxkit.packages | xargs apk add
